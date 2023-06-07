db1c5bc65cdb03b3f057fc08290c27d5254ee109
mohsindev369 <mohsinakrambukhari@gmail.com>
2023-06-07 21:04:40 +0500

    nav almost don

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,14 +1,21 @@
+import { Input } from "./ui/input";
+
 const Header = () => {
   return (
-    <nav className="flex mt-8 mx-4">
-      <div>LOGO</div>
-      <div><ul className="flex gap-12">
-        <li>Male</li>
-        <li>Femail</li>
-        <li>Kids</li>
-        <li>All Product</li>
-      </ul></div>
-    </nav>
+    <header className="flex mt-8 mx-4 sm:justify-evenly max-sm:justify-between items-center gap-2">
+      <div className="text-lg mr-4">LOGO</div>
+      <nav className="max-sm:hidden">
+        <ul className="flex gap-12 truncate ">
+          <li>Male</li>
+          <li>Femail</li>
+          <li>Kids</li>
+          <li>All Product</li>
+        </ul>
+      </nav>
+      {/* <div className="mr-4">Search</div> */}
+      <Input type="text" className="sm:w-1/4" placeholder="Search" />
+      <div>Cart</div>
+    </header>
   );
 };
 
