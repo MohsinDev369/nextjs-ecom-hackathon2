843a7e104c03f3f71d0fb04dc84c1cf778225904
mohsindev369 <mohsinakrambukhari@gmail.com>
2023-06-07 20:35:44 +0500

    working on nav

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,7 @@
-
-
 export default function Home() {
   return (
     <main>
-      e-com
+      <div>page</div>
     </main>
-  )
+  );
 }
---
+++ src/components/Header.tsx
@@ -0,0 +1,15 @@
+const Header = () => {
+  return (
+    <nav className="flex mt-8 mx-4">
+      <div>LOGO</div>
+      <div><ul className="flex gap-12">
+        <li>Male</li>
+        <li>Femail</li>
+        <li>Kids</li>
+        <li>All Product</li>
+      </ul></div>
+    </nav>
+  );
+};
+
+export default Header;
