bb58167535c1572319c16e45f781a309c9b350dc
mohsindev369 <mohsinakrambukhari@gmail.com>
2023-06-17 22:00:17 +0500

    BrandFeature Heading Don

--- src/components/BrandFeautres.tsx
+++ src/components/BrandFeautres.tsx
@@ -1,8 +1,25 @@
-
 const BrandFeautres = () => {
   return (
-    <div>BrandFeautres</div>
-  )
-}
+    <section className="mt-24 px-24 ">
+      <div className="flex justify-center">
+        <div id="title" className="flex xl:justify-end  sm:p-8">
+          <h1 className="font-bold text-[2.75rem] leading-[55px] tracking-[0.03em] sm:w-[80%] xl:w-[45%] text-[#212121]">
+            Unique and Authentic Vintage Designer Jewellery
+          </h1>
+        </div>
+      </div>
+      <div id="feature" className="flex">
+        <div>
+          <div>Different from others</div>
+          <div>1</div>
+          <div>2</div>
+          <div>3</div>
+          <div>4</div>
+        </div>
+        <div>image nad text</div>
+      </div>
+    </section>
+  );
+};
 
-export default BrandFeautres
+export default BrandFeautres;
