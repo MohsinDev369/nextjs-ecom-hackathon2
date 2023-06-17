2033f84cd5d46bd256a654a5dc9dc6249424313e
mohsindev369 <mohsinakrambukhari@gmail.com>
2023-06-17 21:05:28 +0500

    fixed some responsive break points

--- src/components/Promo.tsx
+++ src/components/Promo.tsx
@@ -2,8 +2,8 @@ import Image from "next/image";
 
 const Promo = () => {
   return (
-    <section className="flex flex-col justify-center items-center p-8 gap-4">
-      <span className="text-center text-xs font-bold text-[#0062f5] tracking-widest">
+    <section className="flex flex-col justify-center items-center p-8 gap-4 mt-32 text-center overflow-hidden">
+      <span className="text-xs font-bold text-[#0062f5] tracking-widest leading-10">
         PROMOTIONS
       </span>
       <h2 className="tracking-[.03em] font-bold text-[32px] text-[#212121]">
@@ -11,9 +11,9 @@ const Promo = () => {
       </h2>
       <div className="flex gap-4 max-md:flex-col">
         <div className="flex flex-col gap-4 ">
-          <div className="flex items-center bg-[#d6d6d8] px-8 gap-2">
-            <div >
-              <h3 className="font-bold text-3xl">GET UP TO 60%</h3>
+          <div className="flex items-center bg-[#d6d6d8] px-8 gap-2 max-sm:flex-col">
+            <div>
+              <h3 className="font-bold text-3xl max-md:pt-8">GET UP TO 60%</h3>
               <p className="text-lg">For the summer season</p>
             </div>
             <Image
@@ -21,7 +21,6 @@ const Promo = () => {
               alt="event1"
               width={200}
               height={200}
-              
             />
           </div>
           <div className="flex justify-center items-center flex-col py-12 px-8 bg-[#212121] text-white">
@@ -34,8 +33,8 @@ const Promo = () => {
             </div>
           </div>
         </div>
-        <div className="flex gap-4">
-          <div className="bg-[#efe1c7] relative">
+        <div className="flex gap-4 max-sm:flex-col">
+          <div className="bg-[#efe1c7] relative flex flex-col items-center">
             <div className="my-8 mx-4">
               <p className="">Flex Sweatshirt</p>
               <div className="flex gap-4">
@@ -51,7 +50,7 @@ const Promo = () => {
               className="md:absolute md:bottom-0"
             />
           </div>
-          <div className="bg-[#d7d7d9] relative">
+          <div className="bg-[#d7d7d9] relative flex flex-col items-center">
             <div className="my-8 mx-4">
               <p className="">Flex Sweatshirt</p>
               <div className="flex gap-4">
