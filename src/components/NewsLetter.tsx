b491d989273c3542e561e3b0645b51dc45f45663
mohsindev369 <mohsinakrambukhari@gmail.com>
2023-06-18 17:32:51 +0500

    News letter section don

--- src/components/NewsLetter.tsx
+++ src/components/NewsLetter.tsx
@@ -1,20 +1,24 @@
+import { Button } from "./ui/button";
+import { Input } from "./ui/input";
+
 const NewsLetter = () => {
   return (
-    <section>
-      <div
-        id="NewsLetter"
-        className="flex flex-col justify-center items-center text-center relative my-40 mx-32 "
-      >
-        <div className="font-extrabold absolute text-8xl text-[#f2f3f7] -z-[1] max-sm:text-[3rem]">
-          Newsletter
-        </div>
-        <h1 className="mb-4 font-bold text-4xl tracking-[0.03em] text-[#212121]">
-          Subscribe Our Newsletter
-        </h1>
-        <p className="mb-8 font-light text-base text-[#212121]">
-          Get the latest information and promo offers directly
-        </p>
-        <div>Emil Here</div>
+    <section
+      id="NewsLetter"
+      className="max-sm:my-24 max-sm:mx-8 flex flex-col justify-center items-center text-center relative z-[1]"
+    >
+      <div className="font-extrabold absolute text-8xl text-[#f2f3f7] -z-[1] max-sm:text-[3rem]">
+        Newsletter
+      </div>
+      <h1 className="mb-4 font-bold text-4xl tracking-[0.03em] text-[#212121]">
+        Subscribe Our Newsletter
+      </h1>
+      <p className="mb-8 font-light text-base text-[#212121]">
+        Get the latest information and promo offers directly
+      </p>
+      <div>
+        <Input placeholder="Email Adress" className="mb-4"/>
+        <Button>Get Started</Button>
       </div>
     </section>
   );
