f7f6816b578189294d3bd1221554c54497126717
mohsindev369 <mohsinakrambukhari@gmail.com>
2023-08-03 20:14:09 +0500

    revalidate button changed

--- src/components/RevalidateComponent.tsx
+++ src/components/RevalidateComponent.tsx
@@ -5,7 +5,7 @@ import { Button } from "@/components/ui/button";
 export default function RevalidateComponent() {
     async function HandleRevalidate() {
       await fetch(
-        `https://mohsindev369-ecom-ui.vercel.app/api/revalidate?tag=collection&secret=${process.env.MY_SECRET_TOKEN}`
+        `https://mohsindev369-ecom-ui.vercel.app/api/revalidate?path=/&secret=${process.env.MY_SECRET_TOKEN}`
       );
     }
     return <Button onClick={HandleRevalidate}>Revalidate</Button>
