cfdcc7cbcca684c78ac763f9298d040e1a3d7350
mohsindev369 <mohsinakrambukhari@gmail.com>
2023-08-03 19:45:11 +0500

    revalidate fetch is localhost now actual production url

--- src/components/RevalidateComponent.tsx
+++ src/components/RevalidateComponent.tsx
@@ -5,7 +5,7 @@ import { Button } from "@/components/ui/button";
 export default function RevalidateComponent() {
     async function HandleRevalidate() {
       await fetch(
-        `http://localhost:3000/api/revalidate?tag=collection&secret=${process.env.MY_SECRET_TOKEN}`
+        `https://mohsindev369-ecom-ui.vercel.app/api/revalidate?tag=collection&secret=${process.env.MY_SECRET_TOKEN}`
       );
     }
     return <Button onClick={HandleRevalidate}>Revalidate</Button>
