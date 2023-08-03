dfa070363866ceb3367c1cc2b29a105d98a6271a
mohsindev369 <mohsinakrambukhari@gmail.com>
2023-08-03 21:04:08 +0500

    env refined

--- src/components/RevalidateComponent.tsx
+++ src/components/RevalidateComponent.tsx
@@ -1,12 +1,15 @@
 'use client'
 import { Button } from "@/components/ui/button";
+import { METHODS, get } from "http";
 
 
 export default function RevalidateComponent() {
     async function HandleRevalidate() {
-      await fetch(
-        `https://mohsindev369-ecom-ui.vercel.app/api/revalidate?path=/&secret=${process.env.NEXT_PUBLIC_MY_SECRET_TOKEN}`
+      let req =await fetch(
+        `https://mohsindev369-ecom-ui.vercel.app/api/revalidate?path=/&secret=${process.env.NEXT_PUBLIC_MY_SECRET_TOKEN}`, 
       );
+      let res = await req.json()
+      console.log(res);
     }
     return <Button onClick={HandleRevalidate}>Revalidate</Button>
 }
