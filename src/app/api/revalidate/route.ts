d0664178dd697db4ec767605a05b251bc0847378
mohsindev369 <mohsinakrambukhari@gmail.com>
2023-08-03 20:13:23 +0500

    revalidate tag to path

--- src/app/api/revalidate/route.ts
+++ src/app/api/revalidate/route.ts
@@ -1,9 +1,9 @@
 import { NextApiResponse } from 'next'
 import { NextRequest, NextResponse } from 'next/server'
-import { revalidateTag } from 'next/cache'
+import { revalidatePath } from 'next/cache'
 
 // e.g a webhook to `your-website.com/api/revalidate?tag=collection&secret=<token>`
-// e.g a webhook to `http://localhost:3000/api/revalidate?tag=collection&secret=<token>`
+// e.g a webhook to `http://localhost:3000/api/revalidate?path=/&secret=<token>`
 export async function GET(request: NextRequest, res: NextApiResponse) {
     // Check for secret to confirm this is a valid request
     if (
@@ -12,8 +12,8 @@ export async function GET(request: NextRequest, res: NextApiResponse) {
         return res.status(401).json({ message: 'Invalid token' })
     }
 
-    const tag = request.nextUrl.searchParams.get('tag')
-    tag && revalidateTag(tag)
+    const path = request.nextUrl.searchParams.get('path')
+    path && revalidatePath(path)
     // console.log("revalidate");
     return NextResponse.json({ revalidated: true, now: Date.now() })
 }
