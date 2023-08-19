9e96e6b9a8da1a6abd0b22c76b291868ea79e248
mohsindev369 <mohsinakrambukhari@gmail.com>
2023-08-19 18:47:42 +0500

    added limit on feature product

--- src/components/ui/slider.tsx
+++ src/components/ui/slider.tsx
@@ -9,25 +9,29 @@ const slider = async () => {
    return (
      <div className="">
        <div className="flex gap-4 flex-wrap">
-         {products && products.map((product:any, index:any) => (
-           <div key={index}>
-             <Link href={`/product/${product.slug}`}>
-               <Image
-                 src={product.src}
-                 alt={product.name}
-                 width={200}
-                 height={200}
-                 className=""
-               />
-               <div className="product-info">
-                 <h3 className="text-lg font-bold">{product.name}</h3>
-                 {product.price && (
-                   <p className="text-gray-500">${product.price}</p>
-                 )}
-               </div>
-             </Link>
-           </div>
-         ))}
+         {products &&
+           products.map(
+             (product: any, index: any) =>
+               index < 3 && (
+                 <div key={index}>
+                   <Link href={`/product/${product.slug}`}>
+                     <Image
+                       src={product.src}
+                       alt={product.name}
+                       width={200}
+                       height={200}
+                       className=""
+                     />
+                     <div className="product-info">
+                       <h3 className="text-lg font-bold">{product.name}</h3>
+                       {product.price && (
+                         <p className="text-gray-500">${product.price}</p>
+                       )}
+                     </div>
+                   </Link>
+                 </div>
+               )
+           )}
        </div>
      </div>
    );
